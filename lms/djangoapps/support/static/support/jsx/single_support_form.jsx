e4b832c8efb68cbd1d641c7f3870d67eb57f5bb8
tasawernawaz <tasawer.nawaz@arbisoft.com>
2017-10-26 12:34:26 +0500

    add success component on successfull form submission LEARNER-2735

--- lms/djangoapps/support/static/support/jsx/single_support_form.jsx
+++ lms/djangoapps/support/static/support/jsx/single_support_form.jsx
@@ -10,13 +10,13 @@ import FileUpload from './file_upload';
 import ShowErrors from './errors_list';
 import LoggedInUser from './logged_in_user';
 import LoggedOutUser from './logged_out_user';
+import Success from './success';
 
 // TODO
 // edx zendesk APIs
 // access token
 // custom fields ids
 // https://openedx.atlassian.net/browse/LEARNER-2736
-// https://openedx.atlassian.net/browse/LEARNER-2735
 
 class RenderForm extends React.Component {
   constructor(props) {
@@ -24,6 +24,7 @@ class RenderForm extends React.Component {
     this.state = {
       currentRequest: null,
       errorList: [],
+      success: false,
     };
     this.submitForm = this.submitForm.bind(this);
     this.setErrorState = this.setErrorState.bind(this);
@@ -36,11 +37,11 @@ class RenderForm extends React.Component {
   }
 
   submitForm() {
-    const url = 'https://arbisoft.zendesk.com/api/v2/tickets.json',
+    const url = 'https://example.zendesk.com/api/v2/tickets.json',
       $userInfo = $('.user-info'),
       request = new XMLHttpRequest(),
       $course = $('#course'),
-      accessToken = 'd6ed06821334b6584dd9607d04007c281007324ed07e087879c9c44835c684da',
+      accessToken = 'abc000',
       data = {
         subject: $('#subject').val(),
         comment: {
@@ -78,11 +79,11 @@ class RenderForm extends React.Component {
 
       request.onreadystatechange = function success() {
         if (request.readyState === 4 && request.status === 201) {
-          // TODO needs to remove after implementing success page
-          const alert = 'Request submitted successfully.';
-          alert();
+          this.setState({
+            success: true,
+          });
         }
-      };
+      }.bind(this);
 
       request.onerror = function error() {
         this.setErrorState([gettext('Something went wrong. Please try again later.')]);
@@ -118,7 +119,17 @@ class RenderForm extends React.Component {
     return false;
   }
 
-  render() {
+  renderSuccess() {
+    return (
+      <Success
+        homepageUrl={this.props.context.homepageUrl}
+        dashboardUrl={this.props.context.dashboardUrl}
+        isLoggedIn={this.props.context.user && true}
+      />
+    );
+  }
+
+  renderSupportForm() {
     let userElement;
     if (this.props.context.user) {
       userElement = <LoggedInUser userInformation={this.props.context.user} />;
@@ -194,6 +205,14 @@ class RenderForm extends React.Component {
         </div>
       </div>
     );
+  }
+
+  render() {
+    if (this.state.success) {
+      return this.renderSuccess();
+    }
+
+    return this.renderSupportForm();
   }
 }
 
