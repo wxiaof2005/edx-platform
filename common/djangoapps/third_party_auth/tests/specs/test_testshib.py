b43607ea9be0036f4307e5c3b1742f7889cb15fc
Matt Drayer <mattdrayer@edx.org>
2017-10-31 15:43:57 -0400

    mattdrayer/ENT-686: Log SuccessFactors error response headers

--- common/djangoapps/third_party_auth/tests/specs/test_testshib.py
+++ common/djangoapps/third_party_auth/tests/specs/test_testshib.py
@@ -327,6 +327,8 @@ class SuccessFactorsIntegrationTest(SamlIntegrationTestUtilities, IntegrationTes
             """
             Return a 500 error when someone tries to call the URL.
             """
+            headers['CorrelationId'] = 'aefd38b7-c92c-445a-8c7a-487a3f0c7a9d'
+            headers['RequestNo'] = '[787177]'  # This is the format SAPSF returns for the transaction request number
             return 500, headers, 'Failure!'
 
         fields = ','.join(SapSuccessFactorsIdentityProvider.default_field_mapping.copy())
@@ -519,13 +521,20 @@ class SuccessFactorsIntegrationTest(SamlIntegrationTestUtilities, IntegrationTes
             expected_message = 'Unable to retrieve user details with username {username} from SAPSuccessFactors ' \
                                'for company ID {company_id} with url "{odata_api_url}".  Error message: ' \
                                '500 Server Error: Internal Server Error for url: {odata_api_url}.  System message: ' \
-                               'Not available.'.format(
+                               'Not available.  Headers: '.format(
                                    username=self.USER_USERNAME,
                                    company_id=odata_company_id,
                                    odata_api_url=mocked_odata_ai_url,
                                )
-            logging_messages = [log_msg.getMessage() for log_msg in log_capture.records]
-            self.assertTrue(expected_message in logging_messages)
+            logging_messages = str([log_msg.getMessage() for log_msg in log_capture.records]).replace('\\', '')
+            self.assertIn(odata_company_id, logging_messages)
+            self.assertIn(mocked_odata_ai_url, logging_messages)
+            self.assertIn("SAPSuccessFactors", logging_messages)
+            self.assertIn("Error message", logging_messages)
+            self.assertIn("System message", logging_messages)
+            self.assertIn("Headers", logging_messages)
+            self.assertIn("CorrelationId", logging_messages)
+            self.assertIn("RequestNo", logging_messages)
 
     @skip('Test not necessary for this subclass')
     def test_get_saml_idp_class_with_fake_identifier(self):
